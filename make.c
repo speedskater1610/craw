1cb9a45355f9faa38013f8a7c47385d894979c8e
speedskater1610 <speedskater1610@gmail.com>
2025-07-07 12:09:51 -0400

    to make everything
    cpp files for everything to be compiled into x86 correctly. std.c was old and doesn't compile into anything

---
+++ make.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    const char *command = "g++ main.c read/read.c write/write.c builder/build.c std_compile/std.cpp -o craw.exe";
+
+    int result = system(command);
+
+    if (result == 0) {
+        printf("build successful! Output: craw.exe\n");
+    } else {
+        printf("build failed with code:\n\t %d\n", result);
+    }
+
+    return result;
+}
