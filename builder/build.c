718e2985ba6486d1c8f909ea4e3f276a5828f833
speedskater1610 <speedskater1610@gmail.com>
2025-08-26 08:23:44 -0400

    Update build.c
    cleaned out older version that was commented out

--- builder/build.c
+++ builder/build.c
@@ -1,44 +1,3 @@
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include "build.h"
-
-// void delete_file(char *fileName) {
-//     if (remove(fileName) == 0) {
-//         // file was successfully removed
-//     } else {
-//         perror("Error deleting obj in asm config process file");
-//     }
-// }
-
-// void build_exe_from_asm() { 
-//     int result;
-//     char cmd[512];
-
-//     // build nasm command string
-//     // the out put file is the asm file it is the output of the std_compile in std.c
-//     // nasm -f win64 yourfile.asm -o yourfile.obj
-//     snprintf(cmd, sizeof(cmd), "nasm -f win64 \"%s\" -o hold.obj", output_file);
-//     result = system(cmd);
-
-//     // ERROR handling
-//     if (result != 0) {
-//         printf("ERROR - NASM (CRAW - ASM) failed with code %d\n", result);
-//         return;
-//     }
-
-//     result = system("gcc hold.obj -o final.exe");
-//     if (result != 0) {
-//         printf("ERROR - GCC (ASM - BIN) failed with code %d\n", result);
-//         return;
-//     }
-
-//     delete_file("hold.obj");
-//     //period means it ran fine
-//     printf(".\n");
-// }
-// 
-
-
 #include <cstring>
 #include <stdlib.h>
 #include <stdio.h>
@@ -89,3 +48,4 @@ void build_exe_from_asm(const char *exeFile) {
     //period means it ran fine
     printf("...\n");
 }
+
