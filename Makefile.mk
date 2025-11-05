c05320d6bb80a3fbf1e369d22014ebd3c45c45b8
speedskater1610 <speedskater1610@gmail.com>
2025-11-05 07:47:49 -0500

    Add Makefile.mk

---
+++ Makefile.mk
@@ -0,0 +1,16 @@
+CC = gcc
+CFLAGS = -Wall -g
+TARGET = craw
+SOURCES = src/main.c src/f32.c src/f64.c src/lexer.c src/throwErr.c src/token.c src/vector.c
+OBJECTS = $(SOURCES:.c=.o) # Automatically creates main.o func.o
+
+all: $(TARGET)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $(OBJECTS) -o $(TARGET)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(TARGET) $(OBJECTS)
