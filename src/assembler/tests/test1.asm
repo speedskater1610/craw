c9c37a2e9861c1c1c67cc0a42ea08367d6120967
speedskater1610 <speedskater1610@gmail.com>
2025-11-12 17:50:03 -0500

    Create test1.asm

---
+++ src/assembler/tests/test1.asm
@@ -0,0 +1,22 @@
+_start:
+    jmp setup
+
+setup:
+    ; as ascii vals
+    push 0x0A21
+    push 0x6948
+
+    ; stdout
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, esp   ; pointer to string on stack
+    mov edx, 4
+    int 0x80
+
+    ; clean up the stack
+    add esp, 8
+
+    ; exit
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
