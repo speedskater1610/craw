4f764226438a7d9f4a747ef3fd3e9a804f049bf3
speedskater1610 <speedskater1610@gmail.com>
2025-11-12 14:02:22 -0500

    Update Makefile.mk: Add support for C++ files and properly link them for the exacutable

--- Makefile.mk
+++ Makefile.mk
@@ -1,16 +1,48 @@
+# compilers and flags
 CC = gcc
+CPP = g++
 CFLAGS = -Wall -g
-TARGET = craw
-SOURCES = src/main.c src/throwErr.c src/lexer/token.c src/lexer/vector.c src/lexer/f32.c src/lexer/f64.c src/lexer/lexer.c src/tag/tag.c
-OBJECTS = $(SOURCES:.c=.o) # Automatically creates object files
+CPPFLAGS = -Wall -g
 
+# Target and source files
+TARGET = craw
+
+C_SOURCES = src/main.c \
+            src/throwErr.c \
+            src/lexer/token.c \
+            src/lexer/vector.c \
+            src/lexer/f32.c \
+            src/lexer/f64.c \
+            src/lexer/lexer.c \
+            src/tag/tag.c \
+            src/preprocesser.c
+
+CPP_SOURCES = src/assembler/assembler.cpp
+
+
+# Automatically create object from file lists
+C_OBJECTS = $(C_SOURCES:.c=.o)
+CPP_OBJECTS = $(CPP_SOURCES:.cpp=.o)
+
+# Combine all objects for linking
+OBJECTS = $(C_OBJECTS) $(CPP_OBJECTS)
+
+# Default target
 all: $(TARGET)
 
+# linking the final executable
 $(TARGET): $(OBJECTS)
-	$(CC) $(CFLAGS) $(OBJECTS) -o $(TARGET)
+	$(CPP) $(CPPFLAGS) $(OBJECTS) -o $(TARGET)
 
+# compiling C files
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
+# compiling C++ files
+%.o: %.cpp
+	$(CPP) $(CPPFLAGS) -c $< -o $@
+
+# Phony target for cleaning
+.PHONY: clean
 clean:
 	rm -f $(TARGET) $(OBJECTS)
