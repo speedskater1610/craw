cd6d05a687ee07f6a8724545bdb373193be20f3a
speedskater1610 <speedskater1610@gmail.com>
2025-11-13 07:41:13 -0500

    Update preprocesser.c: add char *escape_char_for_output(char c);

--- src/preprocess/preprocesser.c
+++ src/preprocess/preprocesser.c
@@ -3,3 +3,24 @@
 char *preprocess(char *source) {
 
 }
+
+
+
+char *escape_char_for_output(char c) {
+    switch (c) {
+      case '\n': return "\\n";
+      case '\t': return "\\t";
+      case '\0': return "\\0";
+      case '\\': return "\\\\";
+      case '\'': return "\\'"
+      default:  
+          break;
+    }
+  
+    char *charPtr;
+    charPtr = (char *)malloc(sizeof(char) * 2);
+    charPtr[0] = c;
+    charPtr[1] = '\0';
+    return;
+      
+}
