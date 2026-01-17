7cca0777b947fe998ffb423bf1294d9bf8b71176
speedskater1610 <speedskater1610@gmail.com>
2026-01-17 12:41:48 -0500

    Update ansiColors.h

--- src/ansiColors.h
+++ src/ansiColors.h
@@ -15,7 +15,7 @@
 #define B_YELLOW "\e[1;33m"
 #define B_BLUE "\e[1;34m"
 #define B_MAGENTA "\e[1;35m"
-#define B_CYAN "\e[1;36m"
+#define B_CYAN "\e[1;36m" 
 #define B_WHITE "\e[1;37m"
 
 // underline
