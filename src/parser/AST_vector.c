688337806dadd11a564e244acdd6905009d6bc38
Russel Einsla <speedskater1610@gmail.com>
2026-02-09 08:11:47 -0500

    I want to makee sure this works (workflow)

--- src/parser/AST_vector.c
+++ src/parser/AST_vector.c
@@ -5,3 +5,33 @@
 #include "../throwErr.h"
 #include "../lexer/token.h"
 #include "AST_vector.h"
+
+void vector_free(AST_vector *vector) {
+    if (!vector) return; // NULL case
+
+    if (vector->data) {
+        for (size_t i = 0; i < vector->size; i++) {
+            free(vector->data[i].token);
+            free(vector->data[i].parent);
+            free(vector->data[i].children);
+        }
+
+        free(vector->data);
+    }
+
+    vector->data = NULL;
+    vector->size = 0;
+    vector->capacity = 0;
+
+    free(vector);
+}
+
+// create a new vector
+AST_vector *create_vector() {
+    AST_vector *vector = (AST_vector*)malloc(sizeof(AST_vector));
+    
+    vector->data = NULL;
+    vector->size = 0;
+    vector->capacity = 0;
+    return vector;
+}
